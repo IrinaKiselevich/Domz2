81af7ad5688c4ee43814792e2c414486f4450329
irina <irina.kiselevitch@yandex.ru>
2022-10-08 18:47:33 +0300

    третья  задача

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+int day=6;
+
+if(day<8 && day>0)
+{
+    if(day>5 )
+    {
+        Console.WriteLine("Да");
+    }
+
+    else
+    {
+      Console.WriteLine("Нет");  
+    }
+
+       
+}  
+
+else
+{
+    Console.WriteLine("Такого числа нет");
+}
